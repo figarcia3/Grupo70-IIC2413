9b603e83b2523d40f6d413ed35d779e0928446bf
Fernando <figarcia3@uc.cl>
2020-09-30 15:02:31 -0400

    Test: 1era Consulta

--- Sites/consultas/consulta_tipo.php
+++ Sites/consultas/consulta_tipo.php
@@ -5,8 +5,7 @@
   <?php
   require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
 
-  $var = $_POST["tipo"];
-  $query = "SELECT * FROM pokemones WHERE tipo='$var';";
+  $query = "SELECT * FROM navieras;";
   $result = $db -> prepare($query);
   $result -> execute();
   $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
@@ -16,14 +15,12 @@
     <tr>
       <th>ID</th>
       <th>Nombre</th>
-      <th>Altura</th>
-      <th>Peso</th>
-      <th>Exp Base</th>
-      <th>Tipo</th>
+      <th>Pais</th>
+      <th>Descripcion</th>
     </tr>
   <?php
   foreach ($dataCollected as $p) {
-    echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> </tr>";
+    echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> </tr>";
   }
   ?>
   </table>
