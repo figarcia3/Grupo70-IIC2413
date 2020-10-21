587bf3d0199cc1d745e22aaa1abdefa4f8ebff0d
Fernando <figarcia3@uc.cl>
2020-10-21 01:24:23 -0400

    prueba registration

--- Sites/users/registration.php
+++ Sites/users/registration.php
@@ -5,17 +5,15 @@ require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obti
 $pasaporte = $_POST['pasaporte'];
 $password  = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE pasaporte = '$pasaporte';";
+$query = "SELECT COUNT(*) FROM users WHERE pasaporte = '$pasaporte';";
 
 $result = $db -> prepare($query);
 $result -> execute();
 
-$num = pg_num_rows($result);
-
-if ($num == 0){
-    echo"esta registrado is '$num'";
+if ($result == 0){
+    echo $result "Entro en el 0";
 }else{ 
-    echo"ususario disponilbew y '$num'";
+    echo $result;
 }
 
 ?>
