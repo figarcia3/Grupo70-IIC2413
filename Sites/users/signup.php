a5f9e851d584e9010922d3cfa4ed4d52fe4ccd61
Fernando <figarcia3@uc.cl>
2020-10-26 19:15:06 -0400

    Error de direccionamiento

--- Sites/users/signup.php
+++ Sites/users/signup.php
@@ -14,6 +14,13 @@
 
     if ($stmt->execute()) {
         $message = 'Successfully created new user';
+
+        $records = $db->prepare('SELECT id_user, username, password FROM users WHERE username = :username');
+        $records->bindParam(':username', $_POST['username']);
+        $records->execute();
+        $results = $records->fetch(PDO::FETCH_ASSOC);
+
+        $_SESSION['user_id'] = $results['id_user'];
         header('Location: /~grupo70/index.php');
     } else {
         $message = 'Sorry there must have been an issue creating your account';
