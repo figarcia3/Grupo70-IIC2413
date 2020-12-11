e0fb62454db2d1c1aeddbf44902ea0dd06df9512
DANIELA CASTILLO <dcastillo5@uc.cl>
2020-12-11 00:46:00 -0300

    Mapa avance

--- Sites/pdi/map.js
+++ Sites/pdi/map.js
@@ -1,12 +1,22 @@
-var mymap = L.map('mapid').setView([-22.093103, -70.211854], 13);
+var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    {
+        attribution: false
+    });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+var map = L.map('mapid',
+    {
+        zoomControl: true,
+        layers: [tileLayer],
+    })
+    .setView([0, 	0], 3);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+setTimeout(function () { map.invalidateSize() }, 800);
+
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
--- Sites/pdi/pdi_search_map.php
+++ Sites/pdi/pdi_search_map.php
@@ -2,17 +2,77 @@
 
 <body>
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-   crossorigin=""/>
+
+
+
+<!-- Menu -->
+<div class="topnav">
+  <a href="../users/info_user.php">PERFIL</a>
+  <a href="../MainNavieras.php">NAVIERAS</a>
+  <a href="../otrasconsultas.php">PERSONAS</a>
+  <a href="../message/inbox.php">MENSAJES</a>
+
+</div>
+<br></br>
+<?php 
+if (!empty($_POST['pasaporte'])) {
+
+    $url = 'https://proyectobdd-2020.herokuapp.com/users/'.$_POST['pasaporte'];
+    $result = file_get_contents($url, false);
+    $response = json_decode($result, true);
+    $inbox = $response[0]['inbox_message'];
+    $sent = $response[0]['sent_message'];
+
+
+?>
+<h1> USUARIO ID : <?php echo $_POST['pasaporte'] ?> </h1>
+
+<?php } ?>
+
+<form id ="form" action= "pdi_search_map.php" class="form-container" method="POST">
+
+    <input id = "pasaporte" placeholder="ID..." type="text" name="pasaporte" >
+    <input type="date" id="Date1" name="Date1"  required = "True">
+    <input type="date" id="Date2" name="Date2"  required = "True">
+    <br></br>
+    <input id="desired" placeholder="Búsqueda Simple" type="text" name="desired">
+    <input id="required" placeholder="Busqueda Exacta" type="text" name="required">
+    <input id="forbidden" placeholder="No buscar" type="text" name="forbidden">
+
+    <button type="submit" class="btn" > BUSCAR </button>
+ 
+ 	
+  </form>
+
+
+
  <!-- Make sure you put this AFTER Leaflet's CSS -->
+
+ <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
 
 <div id="mapid" style="height: 600px"></div>
 
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    crossorigin=""></script>
 <script src="map.js"></script>
+
+
+
+<?php 
+if(!empty($inbox)) {
+  foreach ($inbox as $message){ 
+      ?>
+<script> var marker = L.marker([<?php echo $message['lat'] ?>, <?php echo $message['long'] ?>  ]).addTo(map); </script>
+<?php } }?>
+
+
+
+<?php 
+if(!empty($sent)) {
+  foreach ($sent as $message){ 
+       ?>
+<script> var marker = L.marker([<?php echo $message['lat'] ?>, <?php echo $message['long'] ?>  ], {icon: greenIcon}).addTo(map); </script>
+<?php } }?>
 
 
 
