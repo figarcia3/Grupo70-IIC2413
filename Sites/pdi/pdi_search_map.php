c17491b425e9727bd9c39a17efbc751b9a4c4768
Javier Ruiz Valladares <jiruiz5@uc.cl>
2020-12-10 22:18:36 -0300

    pequenos arreglos

--- Sites/pdi/pdi_search_map.php
+++ Sites/pdi/pdi_search_map.php
@@ -1,6 +1,10 @@
 <?php include('../templates/header_pdi.html');   ?>
 
 <body>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
  <!-- Make sure you put this AFTER Leaflet's CSS -->
  <div id="mapid" style="height: 600px"></div>
 
