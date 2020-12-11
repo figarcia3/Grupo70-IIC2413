a079ed6170ba69329740c11d9d19676551fc5e93
Javier Ruiz Valladares <jiruiz5@uc.cl>
2020-12-10 22:44:06 -0300

    hhhhhhh

--- Sites/pdi/pdi_search_map.php
+++ Sites/pdi/pdi_search_map.php
@@ -7,12 +7,13 @@
    crossorigin=""/>
  <!-- Make sure you put this AFTER Leaflet's CSS -->
 
+<div id="mapid" style="height: 600px"></div>
 
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
     integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
     crossorigin=""></script>
 <script src="map.js"></script>
-<div id="mapid" style="height: 600px"></div>
+
 
 </body>
 </html>
